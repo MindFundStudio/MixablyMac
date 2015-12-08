70f616635faba9fb399bd7ea82f1bd613899de24
Leo Tumwattana <leo.tumwattana@gmail.com>
2015-12-08 15:06:03 +0800

    Adds basic popover

--- MixablyMac/AppDelegate.swift
+++ MixablyMac/AppDelegate.swift
@@ -11,16 +11,42 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    let popover = NSPopover()
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        
+        statusItem.title = ""
+        
+        if let button = statusItem.button {
+            button.image = NSImage(named: "StatusBarButtonImage")
+            button.action = Selector("togglePopover:")
+        }
+        
+        popover.contentViewController = MXMainViewController.loadFromNib()
+        
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    func showPopover(sender: NSStatusBarButton) {
+        if let button = statusItem.button {
+            popover.showRelativeToRect(button.bounds, ofView: button, preferredEdge: .MinY)
+        }
+    }
+    
+    func closePopover(sender: NSStatusBarButton) {
+        popover.performClose(sender)
+    }
+    
+    func togglePopover(sender: NSStatusBarButton) {
+        if popover.shown {
+            closePopover(sender)
+        } else {
+            showPopover(sender)
+        }
+    }
 }
 
---
+++ MixablyMac/MXMainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MXMainViewController.swift
+//  MixablyMac
+//
+//  Created by Leo Tumwattana on 8/12/2015.
+//  Copyright © 2015 MiQ. All rights reserved.
+//
+
+import Cocoa
+
+class MXMainViewController: NSViewController {
+    
+    class func loadFromNib() -> MXMainViewController {
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        return storyboard.instantiateControllerWithIdentifier("MXMainViewController") as! MXMainViewController
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
