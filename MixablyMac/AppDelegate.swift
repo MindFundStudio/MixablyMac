bbb161ee9244f01d19517471cde7692da23deea0
Leo Tumwattana <leo.tumwattana@gmail.com>
2015-12-08 14:27:21 +0800

    Init project commit

---
+++ MixablyMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MixablyMac
+//
+//  Created by Leo Tumwattana on 8/12/2015.
+//  Copyright © 2015 MiQ. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MixablyMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MixablyMac
+//
+//  Created by Leo Tumwattana on 8/12/2015.
+//  Copyright © 2015 MiQ. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
