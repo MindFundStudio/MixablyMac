a6d3dbb8fe59ce018903d509d9c1ae83f2454890
Harry Ng <harryworld@gmail.com>
2015-12-10 12:56:43 +0800

    Add dataSource and delegate for NSOutlineView

---
+++ MixablyMac/MXSidebarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MXOutlinePlaylistViewController.swift
+//  MixablyMac
+//
+//  Created by Harry Ng on 9/12/2015.
+//  Copyright © 2015 MiQ. All rights reserved.
+//
+
+import Cocoa
+
+class MXOutlinePlaylistViewController: NSViewController, NSOutlineViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ MixablyMac/Models/Mood.swift
@@ -0,0 +1,19 @@
+//
+//  Mood.swift
+//  MixablyMac
+//
+//  Created by Harry Ng on 10/12/2015.
+//  Copyright © 2015 MiQ. All rights reserved.
+//
+
+import Cocoa
+import RealmSwift
+
+class Mood: Object {
+
+    dynamic var name = "New Playlist"
+    dynamic var order = 0
+    
+    var songs = List<Song>()
+    
+}
