f3e51eadbfb5c5064311f6189bb89b0c4e7008be
Harry Ng <harryworld@gmail.com>
2015-12-12 00:53:05 +0800

    Fix #15, Hook up data source to Realm

---
+++ MixablyMac/MXMixablySongsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MXMixablySongsViewController.swift
+//  MixablyMac
+//
+//  Created by Harry Ng on 12/12/2015.
+//  Copyright © 2015 MiQ. All rights reserved.
+//
+
+import Cocoa
+import RealmSwift
+
+class MXMixablySongsViewController: NSViewController {
+    
+    let realm = try! Realm()
+    dynamic var songs: [Song]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+        
+        songs = realm.objects(Song).map { (song) in return song }
+    }
+    
+}
